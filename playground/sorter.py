98552009c08941554af668876536e5dbb8311830
EC2 Default User <ec2-user@ip-172-31-39-209.ec2.internal>
2014-09-04 18:10:25 +0000

    adding in coversion table for uuids

--- playground/sorter.py
+++ playground/sorter.py
@@ -1,9 +1,20 @@
 
 import parse
+import math
 
 
 
 #print parse.db
+
+def num2deg(xtile, ytile, zoom):
+  n = 2.0 ** zoom
+  lon_deg = xtile / n * 360.0 - 180.0
+  lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+  lat_deg = math.degrees(lat_rad)
+  return (lat_deg, lon_deg)
+
+
+
 
 
 eventTypes = set([record['event']['s'] for record in parse.db])
