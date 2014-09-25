28915f5e8d5fe641d0fc199e2c02dc6a705177f2
EC2 Default User <ec2-user@ip-172-31-39-209.ec2.internal>
2014-09-25 16:11:51 +0000

    fix crit error in type checking

--- playground/munging.py
+++ playground/munging.py
@@ -7,16 +7,16 @@ import csv
 
 
 def getLocations(records):
- if type(records) is type(dict()):
+ if type(records) is type(list()):
   return [(i['x'], i['y']) for i in records if 'x' in i and 'y' in i]
  else:
-  raise TypeError("parameter \'records\' is not type \'dict\'!")
+  raise TypeError("parameter \'records\' is not type \'list\'!")
 
 def writeLocationsToCsv(records, filename):
- if type(records) is type(dict()):
+ if type(records) is type(list()):
   coords = [[i['x'], i['y']] for i in records if 'x' in i and 'y' in i]
   with open(filename, 'wb') as f:
    writer = csv.writer(f)
    writer.writerows(coords)
  else:
-   raise TypeError("parameter \'records\' is not type \'dict\'!")
+   raise TypeError("parameter \'records\' is not type \'list\'!")
