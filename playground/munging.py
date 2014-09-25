7d276a6929d2bcd22b1f399241d004d5789c477f
EC2 Default User <ec2-user@ip-172-31-39-209.ec2.internal>
2014-09-25 16:19:33 +0000

    added ability to clear 0s (by default True)

--- playground/munging.py
+++ playground/munging.py
@@ -6,15 +6,21 @@ import csv
 
 
 
-def getLocations(records):
+def getLocations(records, clearNulls=True):
  if type(records) is type(list()):
-  return [(i['x'], i['y']) for i in records if 'x' in i and 'y' in i]
+  if clearNulls:
+   return [[i['x'], i['y']] for i in records if 'x' in i and 'y' in i and i['x'] is not 0 and i['y'] is not 0]
+  else:
+   return [[i['x'], i['y']] for i in records if 'x' in i and 'y' in i]
  else:
   raise TypeError("parameter \'records\' is not type \'list\'!")
 
-def writeLocationsToCsv(records, filename):
+def writeLocationsToCsv(records, filename, clearNulls=True):
  if type(records) is type(list()):
-  coords = [[i['x'], i['y']] for i in records if 'x' in i and 'y' in i]
+  if clearNulls:
+   coords = [[i['x'], i['y']] for i in records if 'x' in i and 'y' in i and i['x'] is not 0 and i['y'] is not 0]
+  else:
+   coords = [[i['x'], i['y']] for i in records if 'x' in i and 'y' in i]
   with open(filename, 'wb') as f:
    writer = csv.writer(f)
    writer.writerows(coords)
