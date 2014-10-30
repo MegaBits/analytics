72e3b82aac5081586e892501797a6ac7c01ffd8e
EC2 Default User <ec2-user@ip-172-31-39-209.ec2.internal>
2014-10-30 19:57:38 +0000

    basic reporting in

--- playground/reporter.py
+++ playground/reporter.py
@@ -4,14 +4,12 @@ import datetime
 import analysis
 import sorter2
 
-def analyticsReport():
+def analyticsReport(startOfLastPeriod, startDate, endDate):
   
   #Figure out how to programatically generate this given the current date (aka - "yesterday was the last dat of the period I want to inspect")
-  startOfLastPeriod = [13,10,2014]
-  startDate = [20,10,2014]
-  endDate = [27,10,2014]
-
-  
+  #startOfLastPeriod = [13,10,2014]
+  #startDate = [20,10,2014]
+  #endDate = [27,10,2014]  
 
   print("Reports for " + str('/'.join([str(i) for i in startDate])) + " to " + str('/'.join([str(i) for i in endDate])) + " (exclusive)")
   print("========\n")
@@ -21,7 +19,11 @@ def analyticsReport():
   print("User retention")
   print(analysis.userRetention(sorter2.records, startOfLastPeriod, startDate, endDate))
   print("\n========\n")
-  print("Overworld Interactions Per Day (mean)")
-  print(analysis.meanOverworldInteractionsPerDay(sorter2.sortedRecords['locationInteraction'], startDate, endDate))
+  print("Overworld Interactions Per Day for Last 7 days")
+  interactionResults = analysis.meanOverworldInteractionsPerDay(sorter2.sortedRecords['locationInteraction'], startDate, endDate)
+  print("By day:\t\t" + str(interactionResults[0]))
+  print("Mean:\t\t" + str(interactionResults[1]))
+  print("Std dev:\t" + str(interactionResults[2]))
   print("\n========\n")
+ 
 
